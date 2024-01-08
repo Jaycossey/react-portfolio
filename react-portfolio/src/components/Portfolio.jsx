2bceea90295a4d4fa61e74904dfee0d962ca6a37
jaycossey <jaycossey@live.com>
2024-01-08 14:52:40 +0000

    added comments to handle portfolio planning

--- react-portfolio/src/components/Portfolio.jsx
+++ react-portfolio/src/components/Portfolio.jsx
@@ -7,7 +7,18 @@ const Portfolio = () => {
     
     // I need to now work out how to handle the changing of the projects. I can target them with projectList[i]
     // How do I iterate through? and What happens when i is at the length of the array 
+    // I need to have useState ideally to handle which of the projects are shown. I also need to add in the remaining images and screenshots
+    // I need to handle a more dynamic alt tag for the projects images
+    // let's handle the iteration through the arrays first. 
     
+    /**
+     * Current error cases: if i < 1 || i > arr.length - 1;
+     * then data does not exist, 
+     */
+    
+    let left = 0;
+    let center = 1;
+    let right = 2;
     let i = 1;
 
     return (
@@ -19,13 +30,13 @@ const Portfolio = () => {
                 <div className="h-screen flex flex-row gap-20 m-auto text-center items-center justify-center">
                     
                     {/* Portfolio piece [1], click to bring to focus */}
-                    <Presentation position="left" project={projectList[i - 1]} />
+                    <Presentation position="left" project={projectList[left]} />
                     
                     {/* Center focus Portfolio [2] These indexes will be important */}
                     <Presentation position="center" project={projectList[i]} />
 
                     {/* Portfolio [3] 3rd shown half transparent */}
-                    <Presentation position="right" project={projectList[i + 1]} />
+                    <Presentation position="right" project={projectList[right]} />
                 </div>
 
             </div>
