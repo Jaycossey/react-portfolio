dc1eed3ddfd019168be57be2ef42c269fa6a4d64
jaycossey <jaycossey@live.com>
2024-02-17 11:57:38 +0000

    added comments, needs more planning on how the function will work, need to revisit draw method and fillRect

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -2,89 +2,23 @@
 import { useEffect, useRef } from "react";
 
 // Sprites class constructor, takes spriteWidth, spriteHeight, frameCount, frameRate, stateCount, name. as params
-import SpriteFrames from "../utils/sprites";
+import Character from "../utils/character";
 
 const Canvas = (props) => {
-    // create duplicate of framecount and framerate
-    const frameCount = props.frameCount;
-    const frameRate = props.frameRate;
-    const stateCount = props.stateCount;
+    const {name, width, height} = props;
 
-    // spriteSheet details
-    const spriteWidth = props.spriteWidth;
-    const spriteHeight = props.spriteHeight;
-
-    // spritesheet w+h
-    const sheetWidth = spriteWidth * frameCount;
-    const sheetHeight = spriteHeight * stateCount;
-
-    // I need to loop through the total framecount across the sheet
-    // in this case 2 but others will have more and other states -- use OOP to create an object we can pass as an argument
-
-    // spriteFrames
-    const spriteFrames = [];
-    const spritesheet = props.spritesheet;
-
-    for (let i = 0; i < frameCount - 1; i++) {
-        const spriteFrame = new SpriteFrames(spriteWidth, spriteHeight, frameCount, frameRate, stateCount, `${spritesheet}`);
-        // spriteFrames.push(spriteFrame);
-        const sprite = new Image(spriteFrame.spriteHeight, spriteFrame.spriteHeight);
-        sprite.src = spritesheet;
-        spriteFrames.push(sprite);
-    }
-
-    // reference canvas
+    // create ref for current canvas element
     const canvasRef = useRef(null);
 
-    // animate spritesheet
-    const animate = (ctx, currentFrame) => {
-        // animate function here
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        ctx.fillStyle = "green";
-        // draw the frame 
-        ctx.drawImage(spritesheet, spriteFrames[currentFrame].spriteWidth, spriteFrames[currentFrame].spriteHeight, 0, 0, ctx.canvas.width, ctx.canvas.height);
-    }
+    // get current and context (ctx)
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    console.log(ctx);
 
-
-/**
- * 
- *   READ YOUR CODE WHEN YOURE BACK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!READ YOURE CODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!READ YOUR CODE!!!!!!!
- * 
- * go back to drawing board and plan what I need, break it down and compartmentalise everything
- */
-
-
-
-
-    // useEffect for state management - listens to animate function call (allows continuous animation with recursion)
-    useEffect(() => {
-        // canvas reference for targeting 
-        const canvas = canvasRef.current;
-        // canvas context
-        const ctx = canvas.getContext('2d');
-
-        let currentFrame = 0;
-        let animationID;
-        
-        // renderer
-        const renderer = () => {
-            currentFrame++;
-            animate(ctx, currentFrame);
-            animationID = window.requestAnimationFrame(renderer);
-        }
-        renderer();
-
-        // return value
-        return () => window.cancelAnimationFrame(animationID);
-
-    }, [animate]);
+    // I need to handle the name parameter to assign spritesheets
 
     return (
-        <canvas ref={canvasRef}
-                width={props.width} 
-                height={props.height}
-                className="m-auto border-2">
-        </canvas>
+        <canvas ref={canvasRef} width={width} height={height} className="border-2"></canvas>
     );
 }
 
