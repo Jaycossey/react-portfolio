53c1017ce9dec9e0f9e88cf4a1acc067ea061f05
jaycossey <jaycossey@live.com>
2024-02-28 14:40:56 +0000

    updated order of display for portfolio

--- src/utils/projectObj.jsx
+++ src/utils/projectObj.jsx
@@ -5,7 +5,7 @@ const projectList = [
         description: "Website created for P&L Tunes, a music review company, the client requested a mobile first site to display their reviews and staff, as well as including a contact form.",
         image: "/assets/images/pandl.png",
         techStack: "HTML, CSS, JavaScript, TailwindCSS, React.JS",
-        key: "6",
+        key: "1",
         link: "http://pandltunes.co.uk/"
     },
     {
@@ -13,7 +13,7 @@ const projectList = [
         description: "Node project to automate portfolio presentation sites, created for more efficient presentation sites for employers, takes CLI to handle generation.",
         image: "/assets/images/portfolioGen.png",
         techStack: "Node.JS, JavaScript, HTML, CSS, npm",
-        key: "1",
+        key: "2",
         link: "https://github.com/Jaycossey/portfolio-generator"
     },
     {
@@ -21,7 +21,7 @@ const projectList = [
         description: "Node Project to automate README.md file generation using CLI inputs.",
         image: "/assets/images/readmeGen.png",
         techStack: "Node.JS, JavaScript, Markdown",
-        key: "2",
+        key: "3",
         link: "https://github.com/Jaycossey/readme-generator"
     }, 
     {
@@ -29,7 +29,7 @@ const projectList = [
         description: "Node Project to practice file generation, using CLI inputs to generate a team viewer.",
         image: "/assets/images/teamProf.png",
         techStack: "Node.JS, JavaScript, HTML, CSS",
-        key: "3",
+        key: "4",
         link: "https://github.com/Jaycossey/team-profile-gen"
     },
     {
@@ -37,7 +37,7 @@ const projectList = [
         description: "Group project created to display localised crime data for a searched area. Calls the UK police data API.",
         image: "/assets/images/policeInt.png",
         techStack: "HTML, CSS, JavaScript, jQuery",
-        key: "4",
+        key: "6",
         link: "https://github.com/Alan-Chumbley/Police-Interceptors"
     },
     {
